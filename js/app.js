ef19c44a60e4a1efd9ca4b197b1b7d47b31ec439
potisanmetee <iapisak1981@gmail.com>
2019-10-04 20:47:14 -0700

    adds change level logic

--- js/app.js
+++ js/app.js
@@ -31,13 +31,12 @@ $('#player-list').on('submit', function(event) {
     game.push(player)
     const playlistTemplate = `
     <div>
-        <div>Name: ${player.name} Level: ${player.level} Score: ${player.score} Total: ${player.total}</div>
+        <p>${player.name} Score: ${player.score}<p>
         <button id="player-${playerId}">Start Game</button>
     </div>`
     $('#list').append(playlistTemplate)
     $(`#player-${playerId}`).on('click', function() {
         let currentPlayer = this.id.split("-")[1]
-        console.log(currentPlayer)
         startGame(currentPlayer)
     })
 })
@@ -60,17 +59,23 @@ function getLength (lengthOfStartDrop) {
 
 // Create Fruits drop from top to bottom
 // Create div that contain image tag with different picture
-function createFruits () {
+function createFruits (player) {
     // Make new variable to get ID to remove after
     const currentFruitId = createId;   
     let lengthOfStartDrop = Math.floor(Math.random()*fruits.length)
+
     const templateImage = `<image src="${fruits[`${level-1}`].src}" width="50px">`
+
+
     const templateFruits = $(`<div id='${createId}' class='move'>${templateImage}</div>`)
 
-    if (level === 1) {
+
+    // Need to change Logic
+
+    if (game[player].level === 1) {
         templateFruits.addClass('animation-7')
         removeFruits = 7000
-    } else if (level === 2) {
+    } else if (game[player].level === 2) {
         templateFruits.addClass('animation-5')
         removeFruits = 5000
     } else {
@@ -95,26 +100,38 @@ function createFruits () {
     createId++
 }
 
+function checkLevel (player) {
+    
+    if (game[player].level === 1) {
+        templateFruits.addClass('animation-7')
+        removeFruits = 7000
+    } else if (game[player].level === 2) {
+        templateFruits.addClass('animation-5')
+        removeFruits = 5000
+    } else {
+        templateFruits.addClass('animation-3')
+        removeFruits = 3000
+    }
+}
+
 function setTimer (player) {
     const timer = setInterval(()=> {
         time--
         updateTime()
         if (time > 0){
-            createFruits()
+            createFruits(player)
         } else {
             $('#game-board').children().remove()
             game[player].score.push(score)
-            // playerOne.score.push(score)
-            // playerOne.level++
-            // console.log(playerOne)
+            game[player].level++
+
             clearInterval(timer)
-            // $('#main-menu').show()
             $('#main-menu').toggle()
-            if (level>3) {
-                return level=1
-            } else {
+            // if (level>3) {
+            //     return level=1
+            // } else {
                 // level++
-            }   
+            // }   
         }
     }, 800)
 }
@@ -131,7 +148,7 @@ function resetScore () {
 
 function startGame (player) {
     $('#main-menu').toggle()
-    $('#level-section p').text(`Level : ${level}`)
+    $('#level-section p').text(`Level : ${game[player].level}`)
     setTimer(player)
     resetScore()
 }
